a9324b82d3ad4db2b92ac9ec5445048a6bbc2c6d
aaaMsLIUfruit <2337264628@qq.com>
2026-05-11 15:46:21 +0800

    Fix recommend and rank integration startup

--- biteblog-backend/biteblog-recommend/src/main/java/com/biteblog/recommend/RecommendApplication.java
+++ biteblog-backend/biteblog-recommend/src/main/java/com/biteblog/recommend/RecommendApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.biteblog.recommend", "com.biteblog.common"})
 @EnableDiscoveryClient
 @MapperScan("com.biteblog.recommend.mapper")
 public class RecommendApplication {
